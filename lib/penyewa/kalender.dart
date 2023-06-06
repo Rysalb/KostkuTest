730d1087f26700114b9223fffe7625cc217b40f0
Rysa-Laksana-Bhakti <71802110+Rysa-Laksana-Bhakti@users.noreply.github.com>
2023-06-06 21:04:23 +0700

    Nambah Kalender

--- lib/penyewa/kalender.dart
+++ lib/penyewa/kalender.dart
@@ -1,53 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:projectmppl/penyewa/homepenyewa.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Kalender extends StatefulWidget {
-  @override
-  _KalenderState createState() => _KalenderState();
-}
-
-class _KalenderState extends State<Kalender> {
-  CalendarController _calendarController;
-
-  @override
-  void initState() {
-    super.initState();
-    _calendarController = CalendarController();
-  }
-
-  @override
-  void dispose() {
-    _calendarController.dispose();
-    super.dispose();
-  }
+class Kalender extends StatelessWidget {
+  final DateTime _selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Kalender',
-          style: TextStyle(color: Colors.white),
-        ),
         backgroundColor: Colors.orangeAccent[700],
+        elevation: 0,
+        toolbarHeight: 80,
         leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
+          icon: RotatedBox(
+            quarterTurns: 1,
+            child: Icon(
+              Icons.arrow_downward,
+              color: Colors.black,
+              size: 50.0,
+            ),
           ),
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => homepenyewa()),
+            );
           },
         ),
-      ),
-      body: Column(
-        children: [
-          TableCalendar(
-            calendarController: _calendarController,
-            // Konfigurasi opsional lainnya dapat ditambahkan di sini
+        title: Text(
+          'Kalender',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
           ),
-          // Tambahkan widget lain yang diperlukan di sini
-        ],
+        ),
+      ),
+      body: Center(
+        child: TableCalendar(
+          focusedDay: _selectedDate,
+          firstDay: DateTime.utc(2000),
+          lastDay: DateTime.utc(2100),
+          calendarFormat: CalendarFormat.month,
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+          ),
+          calendarStyle: CalendarStyle(
+            todayDecoration: BoxDecoration(
+              color: Colors.orangeAccent,
+              shape: BoxShape.circle,
+            ),
+            selectedDecoration: BoxDecoration(
+              color: Colors.blue,
+              shape: BoxShape.circle,
+            ),
+            selectedTextStyle: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
